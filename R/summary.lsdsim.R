9ff435f3be0220647586bc50fe825759682b74c3
thibautjombart <thibautjombart@gmail.com>
2025-12-20 14:40:36 +0100

    improved infrastructure - digest inputs and labelling

--- R/summary.lsdsim.R
+++ R/summary.lsdsim.R
@@ -12,12 +12,21 @@ summary.lsdsim <- function(object) {
   n_steps <- nrow(x)
   
   ## sum compartments over all populations
-  comps <- c("S", "E", "I", "D", "C", "R", "V")
+  comps <- c("S", "E", "I", "D", "C", "R", "V", "N")
   comp_sums <- list()
   for(e in comps) {
     to_keep <- grep(sprintf("^[%s]", e), colnames(x))
     comp_sums[[e]] <- rowSums(x[, to_keep])
   }
+  
+  ## total cases: including E, I, D, R
+  ## AR is total cases / population size
+  total_cases <- comp_sums[["E"]] + 
+    comp_sums[["I"]] + 
+    comp_sums[["D"]] + 
+    comp_sums[["R"]]
+  ar <- total_cases / comp_sums[["N"]]
+  ar[comp_sums[["N"]] == 0] <- 0
   
   ## number of populations with at least one infectious individual (I):
   ## - n_pop_infec: currently
@@ -35,7 +44,9 @@ summary.lsdsim <- function(object) {
   ## pull results together
   out <- cbind(
     time = seq_len(n_steps), 
-    data.frame(comp_sums), 
+    data.frame(comp_sums),
+    total_cases = total_cases,
+    ar = ar,
     n_pop_infec = n_pop_infec, 
     n_pop_ever_infec = n_pop_ever_infec,
     n_naive = n_naive,
