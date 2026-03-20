ccd33f237f6837c804b8ee384004f3835c69cfde
thibautjombart <thibautjombart@gmail.com>
2026-03-20 17:23:40 +0100

    improving doc

--- R/lsdsim.R
+++ R/lsdsim.R
@@ -6,7 +6,109 @@
 #'
 #' @author Thibaut Jombart \email{thibautjombart@@gmail.com}
 #' @export
+#'
+#' @param grid_size The size of the regular grid to be used. The number of
+#'   populations will be this number squared. Defaults to 1.
+#'
+#' @param time An `integer` indicating the duration of the simulation, in
+#'   days. Defaults to 365.
+#'
+#' @param beta_I The rate of infection for symptomatic cases (I); defaults to
+#'   0.1.
+#'
+#' @param beta_A The rate of infection for asymptomatic cases (A); defaults to
+#'   0.01.
+#'
+#' @param sigma The inverse of the latency period; defaults to 1/7.
+#'
+#' @param gamma The inverse of the duration of illness; defaults to 1/20.
+#'
+#' @param cfr The case fatality ratio; defaults to 0.1 (i.e. 10%).
+#'
+#' @param pasymp The average proportion of asymptomatic cases; defaults to 0.5
+#'   (i.e. 50%).
+#'
+#' @param mass_culling A `logical` indicating if mass culling should be used in
+#'   the response. Only the farm where the response takes place is
+#'   affected. Defaults to FALSE.
 #' 
+#' @param select_culling A `logical` indicating if selective culling should be
+#'   used in the response. Only the farm where the response takes place is
+#'   affected. Defaults to FALSE.
+#' 
+#' @param vaccination A `logical` indicating if vaccination should be used in
+#'   the response. The farm where the response takes place and its neighbours
+#'   are affected. Defaults to FALSE.
+#' 
+#' @param quarantine A `logical` indicating if quarantine should be used in
+#'   the response. The farm where the response takes place and its neighbours
+#'   are affected. Defaults to FALSE.
+#' 
+#' @param insecticide A `logical` indicating if insecticide should be used in
+#'   the response. The farm where the response takes place and its neighbours
+#'   are affected. Defaults to FALSE.
+#'
+#' @param rate_cull The rate at which culling takes place once the response has
+#'   been triggered. Defaults to 1e30, i.e. all animals are killed in 1 day.
+#'
+#' @param vacc_coverage The vaccine coverage, i.e. the proportion of individuals
+#'   getting vaccinated once the response has been triggered. Defaults to 0 - no
+#'   vaccination.
+#'
+#' @param vacc_efficacy The vaccine efficacy, i.e. the proportion of vaccinated
+#'   individuals who effectively become immune. Defaults to 0.65 (i.e. 65%).
+#'
+#' @param quarant_efficacy_in The relative reduction of transmission inside the
+#'   farm due to quarantine. Defaults to 0.2 (i.e. 20%).
+#'
+#' @param quarant_efficacy_out The relative reduction of transmission towards
+#'   neighbouring farms due to quarantine. Defaults to 0.9 (i.e. 90%).
+#'
+#' @param insect_efficacy The relative reduction of transmission due to
+#'   insecticide spray. Defaults to  0.5 (i.e. 50%).
+#'
+#' @param interv_delay The delay to intervention, defined as the number of days
+#'   after the first symptomatic case for the intervention to start. Defaults to
+#'   1e30, i.e. no intervention.
+#'
+#' @param interv_release The number of days after the last symptomatic case
+#'   after which the intervention stops. Defaults to 28 days.
+#'
+#' @param delta The connectivity matrix defining the proportions of the forces
+#'   of infection going from populations (in rows), to populations (in
+#'   columns). Defaults to `NULL`, in which case the matrix is generated
+#'   automatically using rook connectivity and the `diffusion` argument.
+#'
+#' @param diffusion The proportion of the force of infection directed towards
+#'   neighbouring populations, used the build the `delta` matrix if it is set to
+#'   `NULL`. Defaults to 0, i.e. no connectivity (`delta` is the identity
+#'   matrix).
+#'
+#' @param ini_S The initial number of susceptible individuals, recycled as
+#'   needed to match the total number of populations. Defaults to 0.
+#' 
+#' @param ini_E The initial number of exposed individuals, recycled as needed to
+#'   match the total number of populations. Defaults to 0.
+#' 
+#' @param ini_A The initial number of asymptomatic cases, recycled as needed to
+#'   match the total number of populations. Defaults to 0.
+#' 
+#' @param ini_I The initial number of symptomatic cases, recycled as needed to
+#'   match the total number of populations. Defaults to 0.
+#' 
+#' @param ini_C The initial number of culled animals, recycled as needed to
+#'   match the total number of populations. Defaults to 0.
+#' 
+#' @param ini_D The initial number of individuals who died from the disease,
+#'   recycled as needed to match the total number of populations. Defaults to 0.
+#' 
+#' @param ini_R The initial number of recovered individuals, recycled as needed
+#'   to match the total number of populations. Defaults to 0.
+#' 
+#' @param ini_V The initial number of individuals immunized through vaccination,
+#'   recycled as needed to match the total number of populations. Defaults to 0.
+#' 
+
 
 lsdsim <- function(
     grid_size = 1,
